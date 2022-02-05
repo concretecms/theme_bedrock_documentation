8a78c799a1603a5761aaa4579e418d024c9d01c4
Andrew Embler <andrew@concrete5.org>
2022-02-05 12:39:22 -0800

    Adding imagery feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
 import '@concretecms/bedrock/assets/bedrock/js/frontend';
-import '@concretecms/bedrock/assets/imagery/js/frontend';
+
+import 'magnific-popup'
+import '@concretecms/bedrock/assets/imagery/js/frontend/lightbox'
+import '@concretecms/bedrock/assets/imagery/js/frontend/gallery'
+
+// Add Owl Carousel
+import 'owl.carousel'
+$(".owl-carousel").owlCarousel({
+    autoHeight: true,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 450
+});
+
+// Add Simple Parallax to Hero Image
+import simpleParallax from 'simple-parallax-js';
+
+// Add parallax support to all Hero Image blocks
+var images = document.querySelectorAll('.ccm-block-hero-image-image');
+new simpleParallax(images, {scale: 1.5});
+
