c303867f318d688d8d4c692bf4d428b7e9f47a2a
Andrew Embler <andrew@concrete5.org>
2022-02-07 18:34:55 -0800

    Finishing theme customization

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ mix.webpackConfig({
 mix.setPublicPath('themes/flintstone');
 
 mix
-    .sass('assets/scss/skins/default/main.scss', 'css/skins/default.css')
-    .sass('assets/scss/skins/wilma/main.scss', 'css/skins/wilma.css')
+    .sass('themes/flintstone/css/presets/default/main.scss', 'css/skins/default.css')
+    .sass('themes/flintstone/css/presets/wilma/main.scss', 'css/skins/wilma.css')
     .js('assets/js/main.js', 'js/main.js');
